99ecade56c0c69ef48423aaa3ee2dc424f613196
22rithu12 <82137203+22rithu12@users.noreply.github.com>
2021-04-18 18:13:03 +0900

    Add files via upload
    Week 2 Update

--- rithu_capstone.py
+++ rithu_capstone.py
@@ -1,6 +1,7 @@
 import turtle
 import os
 
+#Screen size
 WIDTH = 800
 HEIGHT = 600
 
@@ -16,26 +17,71 @@ pen.speed(0)
 pen.hideturtle()
 pen.penup()
 
+class Game():
+    def __init__(self, width, height):
+        self.width = width 
+        self.height = height
+        
+    def render_border(self, pen):
+        pen.color("white")
+        pen.width(2)
+        pen.penup()
+        
+        left = -self.width/2.0
+        right = self.width/2.0
+        top = -self.height/2.0
+        bottom = self.height/2.0
+        
+        pen.goto(left, top)
+        pen.pendown()
+        pen.goto(right, top)
+        pen.pendown()
+        pen.goto(right, bottom)
+        pen.goto(left, bottom)
+        pen.goto(left, top)
+        pen.penup()
+        
 class Sprite():
     def __init__(self, x, y, shape, color):
         self.direction = 270 
         self.x = x
         self.y = y
+        self.dx = 0
+        self.dy = 0
         self.shape = shape
         self.color = color
-    
+
     def render(self, pen):
         pen.goto(self.x, self.y)
         pen.shape(self.shape)
         pen.color(self.color)
         pen.showturtle()
-        # ~ pen.setheading(self.direction)
         pen.stamp()
         pen.hideturtle()
+        
+    def move(self):
+        self.x += self.dx
+        self.y += self.dy
     
+    def border_check(self):
+        if self.x > game.width/2.0:
+            self.x = game.width/2.0
+            self.dx *= -0.3
+    
+        if self.y > game.height/2.0:
+            self.y = game.height/2.0
+            self.dy *= -0.3
+
+        if self.x < -game.width/2.0:
+            self.x = -game.width/2.0
+            self.dx *= -0.3
+    
+        if self.y < -game.height/2.0:
+            self.y = -game.height/2.0
+            self.dy *= -0.3
         
 
-class Player():
+class Player(Sprite):
     def __init__(self, x, y, shape, color):
         Sprite.__init__(self, x, y, shape, color)
     
@@ -44,57 +90,65 @@ class Player():
         pen.shape(self.shape)
         pen.color(self.color)
         pen.showturtle()
-        # ~ pen.setheading(self.direction)
         pen.stamp()
         pen.hideturtle()
     
     def move_right(self):
-        self.x += 3
-      
+        self.dx = 0.5
+        
     def move_left(self):
-        self.x -= 3 
+        self.dx = -0.5 
         
     def move_up(self):
-        self.y += 3
-        
+        self.dy = 0.5
+    
     def move_down(self):
-        self.y -= 3
-    
-    
-class Puck():
+        self.dy = -0.5
+        
+
+class Puck(Sprite):
     def __init__(self, x, y, shape, color):
         Sprite.__init__(self, x, y, shape, color)   
     
-
+    def render(self, pen):
+        pen.goto(self.x, self.y)
+        pen.shape(self.shape)
+        pen.color(self.color)
+        pen.showturtle()
+        pen.stamp()
+        pen.hideturtle()
+        
+    
 # ~ class Goal():
     # ~ def __init__(self, x, y, shape, color):
         # ~ Sprite.__init__(self, x, y, shape, color)
     
     # ~ def score(self):
         # ~ pass 
-        
 
+game = Game(700, 500)
 
 #forming componets of the game
 player_1 = Player(-300 , 0, "circle", "light yellow")
 
 player_2 = Player(300 , 0, "circle", "light green")
 
-# ~ puck = Puck(Player(0 , 0, "circle", "red")
+puck = Puck(0 , 0, "circle", "red")
 
-sprites = [player_1, player_2]
+sprites = [player_1, player_2, puck]
 
 #keyboard binding
 wn.listen()
-wn.onkeypress(player_1.move_left, "Left")
-wn.onkeypress(player_1.move_right, "Right")
-wn.onkeypress(player_1.move_up, "Up")
-wn.onkeypress(player_1.move_down, "Down")
+wn.onkeypress(player_2.move_left, "Left")
+wn.onkeypress(player_2.move_right, "Right")
+wn.onkeypress(player_2.move_up, "Up")
+wn.onkeypress(player_2.move_down, "Down")
 
-wn.onkeypress(player_2.move_left, "a")
-wn.onkeypress(player_2.move_right, "d")
-wn.onkeypress(player_2.move_up, "w")
-wn.onkeypress(player_2.move_down, "s")
+wn.onkeypress(player_1.move_left, "a")
+wn.onkeypress(player_1.move_right, "d")
+wn.onkeypress(player_1.move_up, "w")
+wn.onkeypress(player_1.move_down, "s")
+
 
 while True:
     wn.update()
@@ -102,7 +156,12 @@ while True:
     
     for sprite in sprites:
         sprite.render(pen)
+        sprite.move()
+        sprite.border_check()
     
+    game.render_border(pen)
+
+    #Check collisions
 
 
 #Create mainloop
